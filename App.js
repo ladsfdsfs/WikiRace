28d09f7196227f19e32b14af060872481fa39c9d
ladsfdsfs <nguyen.k.allen@gmail.com>
2020-03-08 13:29:43 -0400

    established working webview, commit A2

--- App.js
+++ App.js
@@ -1,18 +1,43 @@
 import React, {Component} from 'react';
-import { StyleSheet, Text, View, AsyncStorage, TextInput } from 'react-native';
+import { StyleSheet, Text, View, AsyncStorage, TextInput, processColor, TouchableHighlight} from 'react-native';
 
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 
 import {WebView} from 'react-native-webview';
 
-const Stack = createStackNavigator();
+const Browser = require('react-native-browser')
 
+const Stack = createStackNavigator();
+class MyWeb extends Component {
+  render() {
+    return (<View style={{flex:1}}>
+      <WebView
+        source={{ uri: 'https://m.wikipedia.org' }}
+        style={{ marginTop: 20, width:320, height:80 }}
+      />
+      </View>
+    );
+  }
+}
 
 function HomeScreen({navigation}){
-  return (<View>
+  return (<View style={styles.container}>
+    <View>
     <Text>Start Link:</Text>
     <Text>End Link:</Text>
+    <View>
+      <TouchableHighlight onPress={()=>{
+        console.log('help');
+      }
+    }>
+        <Text>Find link</Text>
+      </TouchableHighlight>
+    </View>
+      
+
+  </View>
+  <MyWeb/>
   </View>)
 }
 function Race({navigation}){
@@ -39,11 +64,18 @@ export default function App() {
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
+
   },
+  video: {
+    marginTop: 20,
+    maxHeight: 200,
+    width: 320,
+    flex: 1
+  }
 });
