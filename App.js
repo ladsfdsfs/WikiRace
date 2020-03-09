bb2754dd0ca302b700c376025b98dffaf186f156
ladsfdsfs <nguyen.k.allen@gmail.com>
2020-03-08 21:39:10 -0400

    working (?) first page, last commit 6a, total work time ~4.5hrs

--- App.js
+++ App.js
@@ -6,15 +6,46 @@ import {createStackNavigator} from '@react-navigation/stack';
 
 import {WebView} from 'react-native-webview';
 
-const Browser = require('react-native-browser')
+async function storeData(key, data){
+  try{
+    await AsyncStorage.setItem(key, data)
+  } catch(e){
+    console.log('nahhh')
+    console.log(e)
+  }
+}
+async function getData(key){
+  try{
+    const value= await AsyncStorage.getItem(key)
+    if(value!=null)
+      return value;
+  } catch(e){
+    console.log('oh no')
+    console.log(e)
+  }
+}
 
 const Stack = createStackNavigator();
 class MyWeb extends Component {
+  storeData = async (key, data)=>{
+    try{
+      await AsyncStorage.setItem(key, data)
+    } catch(e){
+      console.log(e)
+    }
+
+  }
+  _onNavChange(webViewState){
+    let url = webViewState.url;
+    this.storeData('curr', url)
+  }
+  
   render() {
-    return (<View style={{flex:1}}>
+    return (<View style={{flex:1, justifyContent:'center', alignSelf:'center'}}>
       <WebView
+        onNavigationStateChange={this._onNavChange.bind(this)}
         source={{ uri: 'https://m.wikipedia.org' }}
-        style={{ marginTop: 20, width:320, height:80 }}
+        style={styles.web}
       />
       </View>
     );
@@ -23,12 +54,44 @@ class MyWeb extends Component {
 
 function HomeScreen({navigation}){
   return (<View style={styles.container}>
-    <View>
-    <Text>Start Link:</Text>
-    <Text>End Link:</Text>
+    <LinkGetter />
+  <MyWeb/>
+  </View>)
+}
+
+class LinkGetter extends Component{
+  constructor(props){
+    super(props);
+    this.state={start:'<<-- Press to set the current wiki page as this!', end:'<<-- Press to set the current wiki page as this!'}
+  }
+  render(){
+    return(
     <View>
       <TouchableHighlight onPress={()=>{
-        console.log('help');
+        getData('curr').then(val=>{
+          this.setState({start:val})
+          storeData('start', val).then(()=>console.log('yayyy'))
+        })
+        
+      }
+    }>
+      <Text style={styles.button}>Set Start Link:</Text>
+    </TouchableHighlight>
+
+    <TouchableHighlight onPress={()=>{
+      getData('curr').then(val=>{
+        this.setState({end:val})
+        storeData('end', val).then(()=>console.log('i think it worked'))
+      });
+      
+      
+    }}>
+        <Text style={styles.button}>Set End Link:</Text>
+      </TouchableHighlight>
+    
+    <View>
+      <TouchableHighlight onPress={()=>{
+        console.log('Start button here')
       }
     }>
         <Text>Find link</Text>
@@ -37,9 +100,11 @@ function HomeScreen({navigation}){
       
 
   </View>
-  <MyWeb/>
-  </View>)
+    )
+  }
 }
+
+
 function Race({navigation}){
 
   function _onNavChange(webViewState){
@@ -57,7 +122,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="WikiRace Start" component={HomeScreen}/>
+        <Stack.Screen name="WikiRace Start" component={HomeScreen} />
         <Stack.Screen name="WikiRace Go!" component={Race}/>
       </Stack.Navigator>
     </NavigationContainer>
@@ -68,14 +133,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'space-between',
-
+    justifyContent:'space-evenly'
   },
   video: {
     marginTop: 20,
     maxHeight: 200,
     width: 320,
     flex: 1
-  }
+  },
+   web:{
+
+     width:320,
+      height:60 
+   },
+   button:{
+     backgroundColor:'gray',
+     margin:10,
+     fontSize:20,
+     color:'green'
+     
+   }
 });
